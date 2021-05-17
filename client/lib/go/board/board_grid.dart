1ae3973dd2e302ef8148034a8fa50cb65f20c794
le-doude <edouard.pelosi@gmail.com>
2021-05-17 21:31:27 +0900

    Experiment with some refactor

--- client/lib/go/board/board_grid.dart
+++ client/lib/go/board/board_grid.dart
@@ -16,13 +16,14 @@ class BoardGrid extends CustomPainter {
     var frameSize = min(size.height, size.width);
     var gridSize = frameSize * 0.9;
     var gridRect =
-        Rect.fromCenter(center: center, width: gridSize, height: gridSize);
+    Rect.fromCenter(center: center, width: gridSize, height: gridSize);
     BoardIntersections intersections = calculateAllCenters(gridRect);
     drawGridFrom(canvas, intersections);
     drawStarPoints(canvas, intersections);
     var frameRect =
-        Rect.fromCenter(center: center, width: frameSize, height: frameSize);
+    Rect.fromCenter(center: center, width: frameSize, height: frameSize);
     drawGridReference(canvas, intersections, frameRect, gridRect);
+    drawSomeStones(canvas, intersections);
   }
 
   Paint gridInkPaint() {
@@ -56,23 +57,7 @@ class BoardGrid extends CustomPainter {
   void drawCoordinateRefs(Canvas canvas, Rect frameRect) {}
 
   BoardIntersections calculateAllCenters(Rect frame) {
-    var dx = frame.width / this.layout.columns;
-    var dy = frame.height / this.layout.rows;
-    var rect = Rect.fromCenter(
-        center: frame.center,
-        width: frame.width - dx,
-        height: frame.height - dy);
-    var centersColumnsFirst = List.generate(
-        this.layout.columns,
-        (i) => List.generate(this.layout.rows,
-            (j) => Offset(rect.left + i * dx, rect.top + j * dy)),
-        growable: false);
-    var centersRowsFirst = List.generate(
-        this.layout.rows,
-        (j) => List.generate(this.layout.columns,
-            (i) => Offset(rect.left + i * dx, rect.top + j * dy)),
-        growable: false);
-    return BoardIntersections(centersColumnsFirst, centersRowsFirst, dy, dx);
+    return BoardIntersections(frame: frame, layout: this.layout);
   }
 
   void drawGridFrom(Canvas canvas, BoardIntersections centers) {
@@ -93,17 +78,17 @@ class BoardGrid extends CustomPainter {
 
   void drawGridReference(Canvas canvas, BoardIntersections intersections,
       Rect frameRect, Rect gridRect) {
-    var textSize = intersections.cellHeight / 4.5;
+    var textSize = intersections.cellHeight / 2;
     for (int i = 0; i < this.layout.columns; i++) {
       var offset = intersections.get(i, 0);
-      var top = (frameRect.top + gridRect.top) / 2;
+      var top = frameRect.top * 0.75 + gridRect.top  * 0.25;
       var pOffset = Offset(offset.dx - textSize / 3, top);
       drawText(canvas, numberAtCoordinate(i), pOffset, textSize);
     }
 
     for (int i = 0; i < this.layout.rows; i++) {
       var offset = intersections.get(0, i);
-      var left = (frameRect.left + gridRect.left) / 2;
+      var left = frameRect.left * 0.75 + gridRect.left * 0.25;
       var pOffset = Offset(left, offset.dy - textSize / 2);
       drawText(canvas, numberAtCoordinate(i), pOffset, textSize);
     }
@@ -113,26 +98,78 @@ class BoardGrid extends CustomPainter {
     return (i + 1).toString();
   }
 
-  void drawText(
-      Canvas canvas, String numberAtCoordinate, Offset pOffset, double size) {
+  void drawText(Canvas canvas, String numberAtCoordinate, Offset pOffset,
+      double size) {
     var painter = TextPainter(
         text: TextSpan(
             text: numberAtCoordinate,
-            style: TextStyle(color: Colors.black, fontSize: size, fontFamily: "Futura")),
+            style: TextStyle(
+                color: Colors.black, fontSize: size, fontFamily: "Futura")),
         maxLines: 1,
-        textDirection: TextDirection.ltr);
+        textDirection: TextDirection.ltr
+    );
     painter.layout();
     painter.paint(canvas, pOffset);
+  }
+
+  void drawSomeStones(Canvas canvas, BoardIntersections intersections) {
+    var radius = intersections.cellHeight / 2.15;
+    canvas.drawCircle(intersections.get(0, 0), radius, white());
+    canvas.drawCircle(intersections.get(0, 1), radius, black());
+    canvas.drawCircle(intersections.get(5, 0), radius, white());
+    canvas.drawCircle(intersections.get(3, 3), radius, black());
+    canvas.drawCircle(intersections.get(8, 8), radius, white());
+  }
+
+  Paint white() {
+    var paint = Paint();
+    paint.color = Colors.white;
+    paint.style = PaintingStyle.fill;
+    return paint;
+  }
+
+  Paint black() {
+    var paint = Paint();
+    paint.color = Colors.black;
+    paint.style = PaintingStyle.fill;
+    return paint;
   }
 }
 
 class BoardIntersections {
-  final double cellHeight;
-  final double cellWidth;
-  final List<List<Offset>> columns;
-  final List<List<Offset>> rows;
+  Rect frame;
+  Layout layout;
+  List<List<Offset>> columns;
+  List<List<Offset>> rows;
+  double cellWidth;
+  double cellHeight;
 
-  BoardIntersections(this.columns, this.rows, this.cellHeight, this.cellWidth);
+  BoardIntersections({Rect frame, Layout layout}) {
+    this.frame = frame;
+    this.layout = layout;
+    this.cellWidth = frame.width / this.layout.columns;
+    this.cellHeight = frame.height / this.layout.rows;
+    var rect = Rect.fromCenter(
+        center: frame.center,
+        width: frame.width - this.cellWidth,
+        height: frame.height - this.cellHeight);
+    this.columns = List.generate(
+        this.layout.columns,
+            (i) =>
+            List.generate(this.layout.rows,
+                    (j) =>
+                    Offset(rect.left + i * this.cellWidth,
+                        rect.top + j * this.cellHeight)),
+        growable: false);
+    this.rows = List.generate(
+        this.layout.rows,
+            (j) =>
+            List.generate(this.layout.columns,
+                    (i) =>
+                    Offset(rect.left + i * this.cellWidth,
+                        rect.top + j * this.cellHeight)),
+        growable: false);
+  }
 
   Offset get(int x, int y) {
     return this.columns[x][y];
