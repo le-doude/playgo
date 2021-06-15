66d5a9ae113215588612c5e4e7e83a4637bfd3c7
le-doude <edouard.pelosi@gmail.com>
2021-06-15 18:50:21 +0900

    Fix setup for Webflux and websockets

--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -8,8 +8,8 @@ plugins {
 }
 
 group = "org.ledoude"
-version = "0.0.1-SNAPSHOT"
-java.sourceCompatibility = JavaVersion.VERSION_11
+version = "0.0.1"
+java.sourceCompatibility = JavaVersion.VERSION_15
 
 configurations {
     compileOnly {
@@ -22,19 +22,23 @@ repositories {
 }
 
 dependencies {
-    implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-webflux")
-    implementation("org.springframework.boot:spring-boot-starter-websocket")
+    implementation("org.springframework.boot:spring-boot-starter-data-redis-reactive")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
-    implementation("org.junit.jupiter:junit-jupiter:5.7.0")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+
     compileOnly("org.projectlombok:lombok")
+
     developmentOnly("org.springframework.boot:spring-boot-devtools")
+
     annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
     annotationProcessor("org.projectlombok:lombok")
+
+    testImplementation("org.junit.jupiter:junit-jupiter:5.7.0")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("io.projectreactor:reactor-test")
     testImplementation("com.ninja-squad:springmockk:3.0.1")
